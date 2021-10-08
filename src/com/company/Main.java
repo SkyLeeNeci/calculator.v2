01a47d7eada2b93d319afcab95713212e46bea0c
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-08 17:30:24 +0300

    almost final version

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,36 +9,46 @@ public class Main {
         ReadClass readClass = new ReadClass();
         double one;
         double two;
-        System.out.print("Enter first number : ");
-        one = readClass.readNextDouble();
-        System.out.print("Enter second number : ");
-        two = readClass.readNextDouble();
+        int action ;
 
-        Calculator calculator = new Calculator(one, two);
+        do {
+            System.out.print("Enter first number : ");
+            one = readClass.readNextDouble();
+            System.out.print("Enter second number : ");
+            two = readClass.readNextDouble();
 
-        System.out.println("Choose an action : ");
-        System.out.println("1. Add");
-        System.out.println("2. Subs");
-        System.out.println("3. Multi");
-        System.out.println("4. Div");
-        System.out.println("0. Exit");
-        System.out.print("Enter number of action : ");
+            Calculator calculator = new Calculator(one, two);
 
-        int action = (int)readClass.readNextDouble();
+            System.out.println("Choose an action : ");
+            System.out.println("1. Add");
+            System.out.println("2. Subs");
+            System.out.println("3. Multi");
+            System.out.println("4. Div");
+            System.out.println("0. Exit");
+            System.out.print("Enter number of action : ");
 
-        switch (action){
-            case 1: System.out.printf("%.3f%n",calculator.add()); break;
-            case 2:
-                System.out.printf("%.3f%n",calculator.subs());break;
-            case 3:
-                System.out.printf("%.3f%n",calculator.multi());break;
-            case 4:
-                System.out.printf("%.3f%n",calculator.div());break;
-            case 0: break;
-            default:
-                System.out.println("You entered something other ");
+            action = (int) readClass.readNextDouble();
 
-        }
+            switch (action) {
+                case 1:
+                    System.out.printf("%.3f%n", calculator.add());
+                    break;
+                case 2:
+                    System.out.printf("%.3f%n", calculator.subs());
+                    break;
+                case 3:
+                    System.out.printf("%.3f%n", calculator.multi());
+                    break;
+                case 4:
+                    System.out.printf("%.3f%n", calculator.div());
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("You entered something other ");
+
+            }
+        }while (action != 0);
 
     }
 }
